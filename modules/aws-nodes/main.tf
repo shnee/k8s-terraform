f357d3dd5b23ccaa4513db6c672a82f85a931b77
shnee <shnee@shnee.net>
2021-11-12 21:58:22 -0500

    Forgot to add aws-nodes files.
    - Moved network_config.cfg to libvirt-nodes.

---
+++ modules/aws-nodes/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "nodes" {
+  ami                         = var.ami
+  instance_type               = var.ec2-instance-type
+  # key_name                    = aws_key_pair.debug1.key_name
+  associate_public_ip_address = true
+  subnet_id                   = var.subnet-id
+  vpc_security_group_ids = var.security-group-ids
+  user_data = element(var.user-datas.*.rendered, count.index)
+  count                       = var.num-nodes
+
+  tags = {
+    Name = "${var.name-prefix}-${count.index}"
+  }
+}
