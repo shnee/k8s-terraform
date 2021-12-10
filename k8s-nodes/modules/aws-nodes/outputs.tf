073ace27328b06d366a3b24831ef5ac34fe5d4a9
shnee <shnee@shnee.net>
2021-12-10 11:00:45 -0500

    Added support for bridge network interfaces for libvirt.

--- k8s-nodes/modules/aws-nodes/outputs.tf
+++ k8s-nodes/modules/aws-nodes/outputs.tf
@@ -1,3 +1,7 @@
 output "ips" {
   value = aws_instance.nodes.*.public_ip
 }
+
+output "names" {
+  value = aws_instance.nodes.*.tags.Name
+}
--- k8s-nodes/modules/libvirt-nodes/outpus.tf
+++ k8s-nodes/modules/libvirt-nodes/outpus.tf
@@ -1,4 +1,6 @@
 
-output "ips" {
-  value = libvirt_domain.nodes.*.network_interface.0.addresses.0
-}
+# This only works on the default network. They will not work using the bridged
+# network.
+# output "ips" {
+#   value = libvirt_domain.nodes.*.network_interface.0.addresses.0
+# }
