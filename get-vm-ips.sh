fee62e0385254a778ab5a3d0ff3fd9ad07c79ad2
shnee <shnee@shnee.net>
2021-12-09 23:13:00 -0500

    Updated get-vim-ips script to use hostnames.
    - Made the qemu-agent content in the cloud-init config to be controlled
      by a 'install-qemu-agent' flag.

--- get-vm-ips.sh
+++ get-vm-ips.sh
@@ -1,60 +1,92 @@
-#!/bin/sh
+#!/bin/bash
 
-# This script will create environment variables for all of the output IPs. It
-# will also create a `ANSIBLE_INV` variable that will be a comma separated
-# string of all the IPs. A anisble inventory file called "inventory is created
-# as well.
+# This script will create environment variables for all of the output IPs. An
+# anisble inventory file is created as well.
 #
 # Use eval $(./get-vm-ips.sh) to set env vars for ips.
 
 terraform refresh > /dev/null
 
-# All terraform outputs in json format.
-OUTPUTS_JSON="$(
-    terraform show -json | \
-        jq '.values.outputs' | \
-        sed 's/-/_/g')"
-# Just the IP address outputs in json format. Also all '-' characters are
-# replaced by '_' becuase '-' causes jq some problems.
-IPS_JSON="$(
-    echo $OUTPUTS_JSON | \
-        jq 'to_entries | .[] | select(.key | contains("ips"))')"
-# An array of all node "types"
-NODE_TYPE_ARRAY="$(
-    echo $IPS_JSON | \
-        jq '.value.value | to_entries | .[] | .key' | \
+# The file to write the inventory to. This file will be completely overridden.
+INVENTORY_FILE="inventory"
+
+# Grab the the vm name prefix. We do this by greping all *.tfvars files making
+# sure to cat terraform.tfvars last. Then we just grab the last grep result,
+# this way we make sure any value in terraform.tfvars will take priority.
+VM_NAME_PREFIX_VAR="vm-name-prefix"
+VM_NAME_PREFIXES="$( \
+    find . -name "*.tfvars" -exec grep "$VM_NAME_PREFIX_VAR" {} \; && \
+    grep "$VM_NAME_PREFIX_VAR" terraform.tfvars)"
+VM_NAME_PREFIX="$(
+    echo "$VM_NAME_PREFIXES" | \
+        tail -n 1 | \
+        sed 's/^.*=\s*"\(.*\)"/\1/g')"
+
+# This command stores the output data in the format below.
+# [
+#   {
+#     "group": "master",
+#     "vms": [
+#       {
+#         "hostname": "ansible-test-master-0",
+#         "ip": "52.14.114.48"
+#       }
+#     ]
+#   },
+#   {
+#     "group": "worker",
+#     "vms": [
+#       {
+#         "hostname": "ansible-test-worker-0",
+#         "ip": "3.145.121.159"
+#       },
+#       {
+#         "hostname": "ansible-test-worker-1",
+#         "ip": "18.217.112.176"
+#       }
+#     ]
+#   }
+# ]
+DATA="$(terraform show -json | \
+    jq '.values.outputs.groups_hostnames_ips.value | to_entries |
+        map({group: .key, vms:.value | to_entries |
+        map({hostname:.key,ip:.value})})')"
+
+# Pull out the groups from $DATA. The format is a single string with the groups
+# separated by spaces, ie. "group1 group2 group3".
+ANS_GROUPS="$(
+    echo $DATA | \
+        jq '.[] | .group' | \
         sed 's/"//g' | \
-        sed -z 's/\n/ /g;s/ $/\n/g')"
+        tr '\n' ' '
+    )"
 
-# Loop over all the node types and create an export line for each IP.
-VM_IP_EXPORTS="$(
-    for TYPE in $NODE_TYPE_ARRAY; do
+# Clear the inventory file.
+cat /dev/null > $INVENTORY_FILE
 
-        # Convert type, converts "master-ips" to "MASTER"
-        TYPE_UPPER="$(echo ${TYPE^^} | sed s/_.*$//g)"
-        echo "$IPS_JSON" | \
-            jq '.value.value.'"$TYPE"'[]' | \
-            # Add line numbers starting with 0.
-            nl -v 0 | \
-            # Print an export string with a type placeholder "__TYPE__".
-            awk '{print "export __TYPE___" $1 "=" $2}' | \
-            sed s/__TYPE__/$TYPE_UPPER/g
-    done)"
+# For each group, write the VM info to $INVENTORY_FILE and also print a variable
+# expression to stdout.
+for GROUP in $ANS_GROUPS; do
 
-ANSIBLE_INV="$(
-    echo "$VM_IP_EXPORTS" | \
-        sed 's/"//g' | \
-        sed 's/^.*=//g' | \
-        sed -z 's/\n/,/g;s/,$/\n/g')"
+    # Write the inventory file to $INVENTORY_FILE.
+    echo "[$GROUP]" >> $INVENTORY_FILE
+    echo $DATA | \
+        jq '.[] | select(.group=="'"$GROUP"'") | .vms[] | 
+            "\(.hostname) ansible_host=\(.ip)"' | \
+        sed 's/"//g' \
+            >> $INVENTORY_FILE
 
-# Create an inventory file for ansible.
-echo "[k8s_nodes]" > inventory
-echo $VM_IP_EXPORTS | \
-    sed 's/"//g' | \
-    sed 's/export //g' | \
-    sed 's/ /\n/g' | \
-    sed 's/^\(.*\)\(=.*\)$/\1 ansible_host\2/g' \
-        >> inventory
-
-echo $VM_IP_EXPORTS | sed 's/" /"\n/g'
-echo export ANSIBLE_INV=$ANSIBLE_INV
+    # For this group, collect expressions into VARS. The format is:
+    # HOSTNAME1=0.0.0.0
+    # HOSTNAME2=0.0.0.0
+    VARS="$(
+        echo $DATA | \
+            jq '.[] | select(.group=="'"$GROUP"'") | .vms[] | 
+                "\(.hostname)=\(.ip)"' | \
+            sed 's/"//g' | \
+            sed "s/$VM_NAME_PREFIX-//g" | \
+            sed 's/-/_/g'
+        )"
+    # Print the contents of $VARS converted to uppercase.
+    echo "${VARS^^}"
+done
--- main.tf
+++ main.tf
@@ -11,14 +11,15 @@ terraform {
 locals {
   nodes-config = {
     "master" = {
-      base-image = var.amzn2-ami
+      base-image = var.centos8-ami
       num = 1
     },
     "worker" = {
-      base-image = var.amzn2-ami
+      base-image = var.centos8-ami
       num = 2
     }
   }
+  install-qemu-agent = false
 }
 
 ################################################################################
@@ -33,6 +34,7 @@ module "cloud-init-config" {
   num                 = each.value.num
   root-admin-passwd   = var.root-admin-passwd
   root-admin-pub-key  = var.root-admin-pub-key
+  install-qemu-agent  = local.install-qemu-agent
 }
 
 ################################################################################
@@ -125,6 +127,8 @@ module "nodes" {
 # end libvirt
 ################################################################################
 
-output "ips" {
-  value = { for type, node in module.nodes : type => node.ips }
+# This will outpus a map of group => [{hostname, ip}].
+# TODO A 'names' output needs to be added to libvirt.
+output "groups_hostnames_ips" {
+  value = { for type, node in module.nodes : type => zipmap(node.names, node.ips) }
 }
